3fff5ac82901cf84d31dbac186d20ff59e5c4d8c
Animesh Srivastava <animeshsrivastava6199501@gmail.com>
2024-02-03 12:12:08 +0530

    setup s3 bucket for image upload and check rooted device

---
+++ app/src/main/java/com/example/ukbocw/utils/Constant.kt
@@ -0,0 +1,7 @@
+package com.example.ukbocw.utils
+
+object Constant {
+    val ACCESS_ID: String = "AKIATCKATOMYGKLTN27L"
+    val SECRET_KEY: String = "D4E+y+01vuo13ib49z8OjS86dA6k1X6HJLY99GTi"
+    val BUCKET_NAME: String = "ukbocwwb"
+}
